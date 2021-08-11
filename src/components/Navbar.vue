dd0ad809887476d8450e1a63a86101dc64f3ab3c
rizkyputrahidayat <rizkyptr.h@gmail.com>
2021-08-11 17:58:57 +0700

    + Update Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,47 +1,78 @@
 <template>
   <div class="header">
-    <div class="container">
-      <div class="d-flex justify-content-between align-items-center">
-          <div class="col d-flex align-items item-jual">
-          <router-link to="">
-            <img src="../assets/img/mulaijual.jpg" >
-          </router-link>
-          </div>
-          <div class="col d-flex item-jual-1">
-            <router-link to="/login">
-              <img src="../assets/img/login.jpg" >
-            </router-link>
-          </div>
-      </div>
-      <div class="d-flex justify-content-between align-items-center">
-        <div class="d-flex align-items-center">
-          <router-link to="/">
-            <img class="shoope-icon" src="../assets/img/sopi-tes.jpg" >
-          </router-link>
-          <router-link to="/">
-            <img class="search" src="../assets/img/search.jpg" alt="">
-          </router-link>
+        <!-- Navbar -->
+        <div class="container">
+            <div class="d-flex justify-content-between align-items center">
+                <div class="d-flex align-items-center">
+                    <span class="mr-2">Jual</span>
+                    <div class="hr1 mr-2"></div>
+                    <span class="mr-2">Download</span>
+                    <div class="hr1 mr-2"></div>
+                    <span class="mr-2">Ikuti kami di</span>
+                    <img class="icon-media mr-1" src="../assets/img/facebook-circle-logo-24.png" >
+                    <img class="icon-media mr-1" src="../assets/img/instagram-logo-24.png" >
+                    <img class="icon-media mr-1" src="../assets/img/twitter-logo-24.png" >
+                </div>
+                <div class="d-flex align-items-center">
+                    <img class="mr-1 icon-media" src="../assets/img/notification-regular-24.png" >
+                    <span class="mr-3">Notifikasi</span>
+                    <img class="mr-1 icon-media" src="../assets/img/help-circle-regular-24.png" >
+                    <router-link to="/Login" class="details-under">
+                        <span class="mr-3">Login</span>
+                    </router-link>
+                    <div class="hr1 mr-3"></div>
+                    <router-link to="/Login" class="details-under">
+                        <span class="mr-3">Daftar</span>
+                    </router-link>
+                </div>
+            </div>
+
+            <!-- input search -->
+            <div class="d-flex justify-content-between align-items center">
+                <div class="d-flex align-items-center">
+                    <router-link to="/">
+                      <img class="img-shope" src="../assets/img/sopi-tes.jpg" alt="Shoope">
+                    </router-link>
+                </div>
+                <div class="wrap-navbar-input">
+                    <div class="wrap-search">
+                        <input type="text" class="form-control" placeholder="Ekstra Voucher Gratis Ongkir">
+                            <router-link type="button" class="btn btn-search" to="">
+                                <div class="wrap-icon-s">
+                                    <img class="icon-search" src="../assets/img/search-alt-2-regular-24.png" alt="cari">
+                                </div>
+                            </router-link>
+                    </div>
+                    <div class="under-input d-flex mt-2">
+                        <router-link class="details-under" to="/">
+                            <span class="mr-3">Masker</span>
+                        </router-link>
+                        <router-link class="details-under" to="/">
+                            <span class="mr-3">Daster</span>
+                        </router-link>
+                        <router-link class="details-under" to="/">
+                            <span class="mr-3">Masker Kn95</span>
+                        </router-link>
+                        <router-link class="details-under" to="/">
+                            <span class="mr-3">Tas Wanita</span>
+                        </router-link>
+                        <router-link class="details-under" to="/">
+                            <span class="mr-3">Masker Medis</span>
+                        </router-link>
+                        <router-link class="details-under" to="/">
+                            <span class="mr-3">Celana Wanita</span>
+                        </router-link>
+                        <router-link class="details-under" to="/">
+                            <span class="mr-3">Pop It</span>
+                        </router-link>
+                    </div>
+                </div>
+                <router-link to="" type="button">
+                    <img src="../assets/img/cart-regular-24.png" class="icon-cart" alt="shop">
+                </router-link>
+            </div>
         </div>
-        <div class="wrap-search">
-          <input type="text" placeholder="Kolaborasi #SemangatuntukIndonesia" class="form-control">
-          <div class="wrap-icon-s"></div>
-        </div>
-        <!-- <div class="under-input d-flex mt-2">
-          <span>Masker</span>
-          <span class="mr-2">Masker Kf94</span>
-          <span class="mr-2">Daster</span>
-          <span class="mr-2">Masker Kn95</span>
-          <span class="mr-2">Tas Wanita</span>
-          <span class="mr-2">Masker Medis</span>
-          <span class="mr-2">Celana Wanita</span>
-          <span class="mr-2">Pop It</span>
-        </div> -->
-        <route-link to="/">
-          <img class="img-cart" src="../assets/img/cart.png">
-        </route-link>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -52,51 +83,82 @@
 
 <style scoped>
 .header {
-  background: #F54829;
+  background: #f53d2d;
   height: 150px;
   width: 100%;
   position: fixed;
+  color: #f5f0ef;
+  padding-top: 10px;
 }
-.item-jual img{
-  width: 350px;
-  height: 25px;
+
+.hr1 {
+  width: 2px;
+  height: 15px;
+  background-color: #f5f0ef;
 }
-.item-jual-1 img{
-  margin-left: 240px;
-  height: 25px;
+
+.img-shope {
+  width: 190px;
 }
-.wrap-search{
-  position: absolute;
-  width: 670px;
-  height: 40px;
+
+.wrap-search {
+  margin-left: -60px;
+  margin-top: 20px;
+  position: relative;
+  width: 700px;
+  height: 50px;
+  background-color: #f5f0ef;
   border-radius: 12px;
-  margin-left: 280px;
 }
-.form-control{
+.form-control {
   position: absolute;
   width: 100%;
   height: 100%;
   left: 0;
   top: 0;
   border: none;
+  background-color: transparent;
 }
-.img-cart{
-  width: 40px;
+.icon-media {
+  width: 20px;
 }
-.search{
+.wrap-icon-s {
+  background: #f53d2d;
+  border-radius: 8px;
   position: absolute;
-  width: 70px;
-  margin-left: 710px;
-  margin-top: -18px;
+  width: 80px;
+  height: 87%;
+  right: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+  justify-content: center;
+  align-items: center;
 }
-.shoope-icon{
-  margin-top: 10px;
-  margin-left: 20px;
-  width: 220px;
+.wrap-icon-s:hover {
+  background: #f75547;
 }
-span{
-  font-size: 12px;
+.form-control:focus {
+  box-shadow: none;
+  border-radius: 12px;
+}
+.icon-search {
+  width: 30px;
+  margin-left: 5px;
+  margin-top: 7px;
+}
+.details-under {
+  color: white;
+}
+.icon-cart {
+  width: 40px;
+  height: 40px;
+  margin-top: 30px;
+  margin-right: 70px;
+}
+.under-input {
+  margin-left: -50px;
+  font-size: 13px;
   font-weight: 100;
-  /* margin-top: 1000px; */
 }
+
 </style>
